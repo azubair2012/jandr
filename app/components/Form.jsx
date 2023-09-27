078debd6a1cd3be7e86ca340fe8fcb51ec872584
azubair2012 <studio.zubair@gamil.com>
2023-09-27 11:38:24 +0600

    form updated

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -30,7 +30,7 @@ const Form = () => {
   };
 
   return (
-    <div className="w-[350px] h-fit bg-primary rounded-2xl flex items-center flex-col lg:mx-0 mx-[5vw]">
+    <div className="w-[350px] h-fit bg-primary rounded-2xl flex items-center flex-col lg:mx-0 mx-auto px-4">
       <p className="text-4xl font-extrabold pt-4">GET A QUOTE</p>
       <p className="text-xl mb-2">FREE AND FASTER</p>
       <form ref={form} onSubmit={sendEmail} className="flex flex-col">
@@ -67,20 +67,50 @@ const Form = () => {
           placeholder="Phone Number"
           required
         />
+        {/* car detail */}
+        <div className="flex gap-2">
+          <div>
+            <label className="font-bold" htmlFor="phone">
+              Make *
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="car_make"
+              className="w-full px-3 py-2 mb-2 border rounded-md focus:outline-none focus:ring focus:border-black"
+              placeholder="Car make"
+              required
+            />
+          </div>
+          <div>
+            <label className="font-bold" htmlFor="phone">
+              Model *
+            </label>
+            <input
+              type="text"
+              id="phone"
+              name="car_model"
+              className="w-full px-3 py-2 mb-2 border rounded-md focus:outline-none focus:ring focus:border-black"
+              placeholder="Car model"
+              required
+            />
+          </div>
+        </div>
+
         <label className="font-bold" htmlFor="text">
           Your Message *
         </label>
         <textarea
           name="message"
           id="text"
-          className=" resize-none w-[250px] h-[100px] px-2 py-2 rounded-md"
+          className="resize-none h-[100px] px-2 py-2 rounded-md"
           placeholder="Please type in your message here."
           required
         ></textarea>
         <button
           type="submit"
           value="Send"
-          className="w-fit bg-black text-white font-bold text-xl p-2 rounded-lg my-6 mx-auto hover:scale-110"
+          className="w-fit bg-black text-white font-bold text-xl p-2 rounded-lg my-2 mx-auto hover:scale-110"
         >
           GET QUOTE
         </button>
